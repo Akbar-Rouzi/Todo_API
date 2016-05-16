4fbc7e35f2081d16f8f9b86b52d5770b980557c7
Lone Wolf <seagull826@gmail.com>
2016-05-16 13:40:25 -0400

    Add DELETE todos route

--- server.js
+++ server.js
@@ -48,6 +48,21 @@ app.post('/todos', function (req, res) {
     res.json(body);
 });
 
+// Delete/todos/:id
+app.delete('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    
+    if (!matchedTodo) {
+        res.status(404).json({"error" : "no todo found with that id"});
+    } else {
+        //after delteing todo with matched Id ,update todos again.
+        todos = _.without(todos, matchedTodo);
+        // respond with deleted todo
+        res.json(matchedTodo);
+    }
+});
+
 app.listen(PORT, function() {
     console.log("Express listenig on port " + PORT + '!');
 });
